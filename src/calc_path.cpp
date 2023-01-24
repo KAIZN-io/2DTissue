ddfea78b1ae9bf144f78a64eb4333fb52a081836
Jan-Piotraschke <piotraschke.janniklas@gmail.com>
2023-01-24 17:47:18 +0100

    I get now the furthest away vertex of the mesh

--- src/calc_path.cpp
+++ src/calc_path.cpp
@@ -38,6 +38,15 @@ namespace My {
 #define CGAL_GRAPH_TRAITS_INHERITANCE_BASE_CLASS_NAME CGAL::Surface_mesh<::Point_3>
 #include <CGAL/boost/graph/graph_traits_inheritance_macros.h>
 
+typedef boost::graph_traits<My::Mesh>::vertex_descriptor          vertex_descriptor;
+
+// We use a std::map to store the index
+typedef std::map<vertex_descriptor, int>                              VertexIndexMap;
+VertexIndexMap vertex_id_map;
+// A std::map is not a property map, because it is not lightweight
+typedef boost::associative_property_map<VertexIndexMap>               VertexIdPropertyMap;
+VertexIdPropertyMap vertex_index_pmap(vertex_id_map);
+
 
 /*
 ! Solve the graph using the node notation and not the Point_3 notation
@@ -48,61 +57,53 @@ int main(int argc, char** argv)
     std::ifstream in((argc>1)?argv[1]:CGAL::data_file_path("git_repos/Confined_active_particles/meshes/sphere.off"));
     in >> mesh;
 
-    typedef boost::graph_traits<My::Mesh>::vertex_descriptor vertex_descriptor;
+    // typedef boost::graph_traits<My::Mesh>::vertex_descriptor vertex_descriptor;
     typedef boost::property_map<My::Mesh,CGAL::vertex_point_t>::type Point_property_map;
 
     Point_property_map ppm = get(CGAL::vertex_point, mesh);
     // Create vectors to store the predecessors (p) and the distances from the root (d)
-    std::vector<vertex_descriptor> predecessor_pmap(num_vertices(mesh));
-    std::vector<int> d(num_vertices(mesh));
-
-    // Create descriptor for the source node
-    // vertex_descriptor s = boost::vertex(vertices(mesh).first, mesh);
-    //   vertex_descriptor goal = vertex(E, mesh);
-    for(vertex_descriptor vd : vertices(mesh)){
-        if (vd != boost::graph_traits<My::Mesh>::null_vertex()){
-            std::cout << vd << " at " << get(ppm, vd) << std::endl;
-        }
-    }
+    std::vector<vertex_descriptor> predecessor_pmap(num_vertices(mesh));  // record the predecessor of each vertex
+    std::vector<int> distance(num_vertices(mesh));
 
     auto indexmap = get(boost::vertex_index, mesh);
-    std::vector<int> distance(mesh.number_of_vertices());
-    std::cout << "P.size_of_vertices() = " << mesh.number_of_vertices() << std::endl;
+    auto dist_pmap = boost::make_iterator_property_map(distance.begin(), indexmap);
+
+    std::vector<vertex_descriptor> predecessor(num_vertices(mesh));  // We first declare a vector
+    boost::iterator_property_map<std::vector<vertex_descriptor>::iterator, VertexIdPropertyMap> predecessor_pmapa(predecessor.begin(), vertex_index_pmap);  // and then turn it into a property map
+
+    // to use two visitors, you need to put them in a pair (from https://theboostcpplibraries.com/boost.graph-algorithms)
+    auto vis = boost::make_bfs_visitor(std::make_pair(boost::record_distances(dist_pmap, boost::on_tree_edge{}),boost::record_predecessors(predecessor_pmapa, boost::on_tree_edge{})));
+
+    std::cout << "number of vertices: " << mesh.number_of_vertices() << std::endl;
+    std::cout << "number of edges in the mesh: " << mesh.number_of_edges() << std::endl;
+
     vertex_descriptor start_node = *(vertices(mesh).first);
 
 
-    // the following code: DON'T ask me ... I will fix it in the future
+    /*
+    Find the target node
+    */
+    boost::breadth_first_search(mesh, start_node, visitor(vis));
+
+    // Traverse all vertices and show at what distance they are
+    int max_distances = 0;
     decltype(start_node) target_node;
     for(vertex_descriptor vd : vertices(mesh)){
         if (vd != boost::graph_traits<My::Mesh>::null_vertex()){
-            target_node = vd;
+            // std::cout << vd << " at " << get(ppm, vd) << " is " << distance[vd] << " hops away" << std::endl;
+            if (distance[vd] > max_distances) {
+                max_distances = distance[vd];
+                target_node = vd;
+            }
         }
     }
+    std::cout << "max distance: " << max_distances << std::endl;
+    std::cout << "got the following target point: " << target_node << " at " << get(ppm, target_node) << std::endl;
 
-    std::cout << "starting node = " << target_node << std::endl;
-    std::cout << "We compute the route from the following point: " << get(ppm, start_node) << " to " << get(ppm,target_node) <<  std::endl;
 
     // ! From: https://www.technical-recipes.com/2015/getting-started-with-the-boost-graph-library/
     // Evaluate Dijkstra on graph mesh with source start_node, predecessor_map predecessor_pmap and distance_map d
-    boost::dijkstra_shortest_paths(mesh, start_node, boost::predecessor_map(&predecessor_pmap[0]).distance_map(&d[0]));
-
-
-
-    // Polyhedron P;
-    // std::ifstream input((argc>1)?argv[1]:CGAL::data_file_path("git_repos/Confined_active_particles/meshes/sphere.off"));
-    // input >> P ;
-
-    // auto dist_pmap = boost::make_iterator_property_map(d.begin(), indexmap);
-
-    // // record the predecessor of each vertex
-    // std::vector<vertex_descriptor> predecessor(boost::num_vertices(P));  // We first declare a vector
-    // boost::iterator_property_map<std::vector<vertex_descriptor>::iterator, VertexIdPropertyMap> predecessor_pmaps(predecessor.begin(), vertex_index_pmap);  // and then turn it into a property map
-  
-    // auto vis = boost::make_bfs_visitor(std::make_pair(boost::record_distances(&d[0], boost::on_tree_edge{}),boost::record_predecessors(&predecessor_pmaps[0], boost::on_tree_edge{})));
-    // // run the search
-    // boost::breadth_first_search(P, start_node, visitor(vis));
-
-
+    boost::dijkstra_shortest_paths(mesh, start_node, boost::predecessor_map(&predecessor_pmap[0]).distance_map(&distance[0]));
 
 
     /*
@@ -128,10 +129,7 @@ int main(int argc, char** argv)
         path_list_points.push_back(get(ppm, *it));
         hope_number++;
     }
-
-    std::cout << path_list_points[1] << std::endl;
-    std::cout << "\n " << hope_number << std::endl;
-    std::cout << "number of edges in the mesh: " << mesh.number_of_edges() << '\n';
+    std::cout << "hope_number = " << hope_number << std::endl;
 
     // Write the path to a file
     std::ofstream output_file("git_repos/Confined_active_particles/meshes/cut_line.selection.txt");
