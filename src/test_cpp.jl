e8ae6f46a2d8a8a8e966c53b02be478f501dcb05
Jan-Piotraschke <piotraschke.janniklas@gmail.com>
2023-02-16 11:33:16 +0100

    compiling CGAL using Cmake

--- src/test_cpp.jl
+++ src/test_cpp.jl
@@ -1,14 +1,25 @@
-# ! Please note that you need .julia/artifacts/Overrides.toml where you refere to your C++ builts
-
 # Load the module and generate the functions
 module CppHello
   using CxxWrap
-  @wrapmodule(joinpath("/Users/jan-piotraschke/git_repos/Confined_active_particles/build", "lib", "liblibhello"))
+
+  @wrapmodule(joinpath(@__DIR__, "build/lib", "libhello"))
 
   function __init__()
     @initcxx
   end
 end
-
 # Call greet and show the result
 @show CppHello.greet()
+
+
+module HalfFunction
+  using CxxWrap
+
+  @wrapmodule(joinpath(@__DIR__, "build/lib", "libfunc"))
+
+  function __init__()
+    @initcxx
+  end
+end
+@show HalfFunction.half_d(3)
+
