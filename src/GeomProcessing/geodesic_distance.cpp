0c942a3e6019eaee4c4c1590abaf5bc8960a755c
Jan-Piotraschke <piotraschke.janniklas@gmail.com>
2023-03-27 13:13:43 +0200

    improved the exactiness of the heat method

--- src/GeomProcessing/geodesic_distance.cpp
+++ src/GeomProcessing/geodesic_distance.cpp
@@ -3,6 +3,11 @@
 // license: Apache License 2.0
 // version: 0.1.0
 
+/*
+Shortest paths on a terrain using one source point 
+The heat method package returns an Approximation of the Geodesic Distance for all vertices of a triangle mesh to the closest vertex in a given set of source vertices.
+As a rule of thumb, the method works well on triangle meshes, which are Delaunay.
+*/
 
 #include <CGAL/Simple_cartesian.h>
 #include <CGAL/Surface_mesh.h>
@@ -20,16 +25,18 @@
 #include "jlcxx/functions.hpp"
 
 
-typedef CGAL::Simple_cartesian<double>                       Kernel;
-typedef Kernel::Point_3                                      Point_3;
-typedef CGAL::Surface_mesh<Point_3>                          Triangle_mesh;
+using Kernel = CGAL::Simple_cartesian<double>;
+using Point_3 = Kernel::Point_3;
+using Triangle_mesh = CGAL::Surface_mesh<Point_3>;
 
-typedef boost::graph_traits<Triangle_mesh>::vertex_descriptor vertex_descriptor;
-typedef Triangle_mesh::Property_map<vertex_descriptor,double> Vertex_distance_map;
+using vertex_descriptor = boost::graph_traits<Triangle_mesh>::vertex_descriptor;
+using Vertex_distance_map = Triangle_mesh::Property_map<vertex_descriptor, double>;
 
-typedef CGAL::Heat_method_3::Surface_mesh_geodesic_distances_3<Triangle_mesh, CGAL::Heat_method_3::Direct> Heat_method_idt;
+//  The Intrinsic Delaunay Triangulation algorithm is switched off by the template parameter Heat_method_3::Direct.
+using Heat_method_idt = CGAL::Heat_method_3::Surface_mesh_geodesic_distances_3<Triangle_mesh, CGAL::Heat_method_3::Direct>;
+using Heat_method = CGAL::Heat_method_3::Surface_mesh_geodesic_distances_3<Triangle_mesh>;
 
-typedef jlcxx::ArrayRef<double, 1> JuliaArray;
+using JuliaArray = jlcxx::ArrayRef<double, 1>;
 
 
 JuliaArray geo_distance(int32_t start_node)
@@ -39,14 +46,13 @@ JuliaArray geo_distance(int32_t start_node)
     filename >> tm;
 
     //property map for the distance values to the source set
-    Vertex_distance_map vertex_distance = tm.add_property_map<vertex_descriptor,double>("v:distance",0).first;
+    Vertex_distance_map vertex_distance = tm.add_property_map<vertex_descriptor, double>("v:distance", 0).first;
 
     //pass in the idt object and its vertex_distance_map
-    Heat_method_idt hm_idt(tm);
+    Heat_method hm_idt(tm);
 
     //add the first vertex as the source set
     vertex_descriptor source = *(vertices(tm).first + start_node);
-
     hm_idt.add_source(source);
     hm_idt.estimate_geodesic_distances(vertex_distance);
 
