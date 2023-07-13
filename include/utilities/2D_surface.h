8a2aa1b4076f54c21b7653d7d9398e49ca550ed0
Jan-Piotraschke <piotraschke.janniklas@gmail.com>
2023-07-13 17:08:47 +0200

    check the length of the cut line

--- include/utilities/2D_surface.h
+++ include/utilities/2D_surface.h
@@ -4,10 +4,17 @@
 #include <utility>
 #include <vector>
 #include <Eigen/Dense>
+#include <utilities/mesh_descriptor.h>
+
 
 std::tuple<std::vector<int64_t>, Eigen::MatrixXd, Eigen::MatrixXd, std::string> create_uv_surface(
     std::string mesh_file_path,
     int32_t start_node_int
+);
+
+std::vector<_3D::edge_descriptor> set_UV_border_edges(
+    const std::string mesh_file_path,
+    _3D::vertex_descriptor start_node
 );
 
 std::string get_mesh_name(
--- tests/simulation/utilities/test_2D_surface.cpp
+++ tests/simulation/utilities/test_2D_surface.cpp
@@ -4,8 +4,14 @@
 // version: 0.1.0
 
 #include <gtest/gtest.h>
+#include <fstream>
+#include <boost/filesystem.hpp>
 
 #include <utilities/2D_surface.h>
+
+namespace fs = boost::filesystem;
+const fs::path PROJECT_PATH = PROJECT_SOURCE_DIR;
+const fs::path MESH_FOLDER = PROJECT_PATH  / "meshes";
 
 TEST(MeshNameTest, NormalFileName) {
     std::string path = "/path/to/mesh_file.off";
@@ -23,4 +29,21 @@ TEST(MeshNameTest, NoFileName) {
     std::string path = "/path/to/";
     std::string expected = ".";
     EXPECT_EQ(get_mesh_name(path), expected);
+}
+
+TEST(SetUVBorderEdges, Test1) {
+    std::string mesh_file_path = (MESH_FOLDER / "ellipsoid_x4.off").string();
+    int start_node_int = 0;
+
+    // Load the 3D mesh
+    _3D::Mesh sm;
+    std::ifstream in(CGAL::data_file_path(mesh_file_path));
+    in >> sm;
+
+    _3D::vertex_descriptor start_node = *(vertices(sm).first + start_node_int);
+    std::vector<_3D::edge_descriptor> result = set_UV_border_edges(mesh_file_path, start_node);
+
+    // Check the length of the result
+    int expected = 44;
+    EXPECT_EQ(result.size(), expected);
 }
