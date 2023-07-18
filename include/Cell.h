b896e044a8a1641afd2900d6e91de212098f3138
Jan-Piotraschke <piotraschke.janniklas@gmail.com>
2023-07-18 11:56:09 +0200

    typo fix

--- include/Cell.h
+++ include/Cell.h
@@ -7,8 +7,17 @@
 
 #include <IO.h>
 
+// Cell.h
+#pragma once
+
+#include <vector>
+#include <cstdint>
+#include <Eigen/Dense>
+
+#include <IO.h>
+
 class Cell {
-pulbic:
+public:
     Cell(
         int num_part,
         const Eigen::MatrixXd halfedge_UV,
@@ -22,12 +31,14 @@ pulbic:
         Eigen::Matrix<double, Eigen::Dynamic, 2>& r_UV,
         Eigen::VectorXd& n
     );
-    std::pair<Eigen::MatrixXd, std::vector<int>> get_r3d(
-        const Eigen::Matrix<double, Eigen::Dynamic, 2> r_UV
-    );
-    Eigen::Matrix<double, Eigen::Dynamic, 2> get_r2d(
-        const Eigen::MatrixXd r_3D
-    );
+
+    std::pair<Eigen::MatrixXd, std::vector<int>> get_r3d();
+    Eigen::Matrix<double, Eigen::Dynamic, 2> get_r2d();
+
+    // getters
+    Eigen::Matrix<double, Eigen::Dynamic, 2> get_r_UV() const { return r_UV; }
+    Eigen::VectorXd get_n() const { return n; }
+    Eigen::MatrixXd get_r_3D() const { return r_3D; }
 
 private:
     const int num_part;
@@ -35,22 +46,16 @@ private:
     const Eigen::MatrixXi face_UV,
     const Eigen::MatrixXd vertice_UV,
     const Eigen::MatrixXd vertice_3D,
-    std::vector<int64_t> h_v_mapping
+    std::vector<int64_t> h_v_mapping;
 
-    std::pair<Eigen::Vector3d, int> calculate_barycentric_3D_coord(
-        const Eigen::Matrix<double, Eigen::Dynamic, 2> r_UV,
-        int interator
-    );
+    // additional private members
+    Eigen::Matrix<double, Eigen::Dynamic, 2> r_UV;
+    Eigen::VectorXd n;
+    Eigen::MatrixXd r_3D;
 
-    Eigen::Vector3d calculate_barycentric_2D_coord(
-        const Eigen::MatrixXd r_3D,
-        int iterator
-    );
+    std::pair<Eigen::Vector3d, int> calculate_barycentric_3D_coord(int interator);
+    Eigen::Vector3d calculate_barycentric_2D_coord(int iterator);
 };
-
-
-
-
 
 
 
