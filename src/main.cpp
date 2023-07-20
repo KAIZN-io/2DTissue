3b9b21f79f410543a6dc8f0a9c607a31debdb3a4
Jan-Piotraschke <piotraschke.janniklas@gmail.com>
2023-07-20 10:40:36 +0200

    init code of small Sundials example

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,25 @@
 #include <cvode/cvode.h>
 #include <idas/idas.h>
 #include <nvector/nvector_serial.h>
+#include <nvector/nvector_parallel.h>
 #include <sundials/sundials_types.h>
 #include <sundials/sundials_math.h>
+#include <sunmatrix/sunmatrix_dense.h>
+#include <sunlinsol/sunlinsol_dense.h>
+#include <sundials/sundials_types.h>
 
 #include <2DTissue.h>
 
 const boost::filesystem::path PROJECT_PATH = PROJECT_SOURCE_DIR;
+
+// The ODE system
+int f(realtype t, N_Vector y, N_Vector ydot, void *user_data)
+{
+    realtype yval = NV_Ith_S(y, 0);
+    NV_Ith_S(ydot, 0) = -0.5 * yval;
+    return 0;
+}
+
 
 int main()
 {
@@ -24,22 +37,78 @@ int main()
     std::string mesh_path = PROJECT_PATH.string() + "/meshes/ellipsoid_x4.off";
     // std::string mesh_path = PROJECT_PATH.string() + "/meshes/sphere.off";
 
-    for (int particle_count = 200; particle_count <= 200; particle_count += 100) {
-        _2DTissue _2dtissue(save_data, mesh_path, particle_count, step_count, 0.01);  // Initialize the 2DTissue object
+    // for (int particle_count = 200; particle_count <= 200; particle_count += 100) {
+    //     _2DTissue _2dtissue(save_data, mesh_path, particle_count, step_count, 0.01);  // Initialize the 2DTissue object
 
-        _2dtissue.start();
+    //     _2dtissue.start();
 
-        std::clock_t start = std::clock();
+    //     std::clock_t start = std::clock();
 
-        while(!_2dtissue.is_finished()) {
-            System data = _2dtissue.update();
-        }
-        std::cout << _2dtissue.get_order_parameter() << '\n';
+    //     while(!_2dtissue.is_finished()) {
+    //         System data = _2dtissue.update();
+    //     }
+    //     std::cout << _2dtissue.get_order_parameter() << '\n';
 
-        std::clock_t end = std::clock();
-        double duration = (end - start) / (double) CLOCKS_PER_SEC;
-        std::cout << "Time taken: " << duration << " seconds" << '\n';
+    //     std::clock_t end = std::clock();
+    //     double duration = (end - start) / (double) CLOCKS_PER_SEC;
+    //     std::cout << "Time taken: " << duration << " seconds" << '\n';
+    // }
+
+    /*
+    Sundials test code
+    */
+    SUNContext sunctx;
+    void* comm = nullptr;
+    int flag = SUNContext_Create(comm, &sunctx);
+    if (flag != 0) {
+        // handle error
     }
+
+    N_Vector y = N_VNew_Serial(1, sunctx); 
+    realtype t = 0.0;
+    realtype reltol = 1e-4, abstol = 1e-4;  // Set tolerances
+
+    std::cout << "t = " << t << ", y = " << y << std::endl;
+    // Initialize y
+    NV_Ith_S(y, 0) = 1.0;
+
+    // Call CVodeCreate to create the solver memory and specify the
+    // Backward Differentiation Formula and the use of a Newton iteration
+    // void *cvode_mem = CVodeCreate(CV_BDF, CV_NEWTON);
+    
+    // // Initialize the integrator memory and specify the user's right hand
+    // // side function in y'=f(t,y), the inital time T0, and the initial
+    // // dependent variable vector y.
+    // CVodeInit(cvode_mem, f, t, y);
+
+    // // Set the scalar relative tolerance and scalar absolute tolerance
+    // CVodeSStolerances(cvode_mem, reltol, abstol);
+    
+    // // Call CVDense to specify the CVDENSE dense linear solver
+    // SUNMatrix A = SUNDenseMatrix(1, 1); // Create dense matrix for use in linear solves
+    // SUNLinearSolver LS = SUNDenseLinearSolver(y, A); // Create dense linear solver for use in linear solves
+    // CVDlsSetLinearSolver(cvode_mem, LS, A); // Attach the matrix and linear solver
+
+    // // Integrate over the interval, while the test function returns success
+    // realtype tout = 1.0;
+    // while (t < 10.0)
+    // {
+    //     int flag = CVode(cvode_mem, tout, y, &t, CV_NORMAL);
+    //     if (flag < 0)
+    //     {
+    //         std::cerr << "Error in integration" << std::endl;
+    //         return -1;
+    //     }
+    //     std::cout << "At t = " << t << ", y = " << NV_Ith_S(y,0) << std::endl;
+    //     tout *= 2.0;
+    // }
+
+    // Free integrator memory
+    // CVodeFree(&cvode_mem);
+    SUNContext_Free(&sunctx);
+
+    // // Free vector
+    // N_VDestroy(y);
 
     return 0;
 }
