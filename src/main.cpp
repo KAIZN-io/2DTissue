46ac02712006b15af343701d7e5fa381924cac1c
Jan-Piotraschke <piotraschke.janniklas@gmail.com>
2023-07-20 11:42:51 +0200

    code refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -18,15 +18,20 @@
 #include <2DTissue.h>
 
 const boost::filesystem::path PROJECT_PATH = PROJECT_SOURCE_DIR;
+#define NEQ   2                /* number of equations */
 
 // The ODE system
-int f(realtype t, N_Vector y, N_Vector ydot, void *user_data)
+static int simulate_sine(realtype t, N_Vector y, N_Vector ydot, void *user_data)
 {
-    realtype yval = NV_Ith_S(y, 0);
-    NV_Ith_S(ydot, 0) = -0.5 * yval;
+    realtype y_val = NV_Ith_S(y, 0);
+    realtype v = NV_Ith_S(y, 1);
+
+    // Store the data in the ydot vector
+    NV_Ith_S(ydot, 0) = v;
+    NV_Ith_S(ydot, 1) = -y_val;
+
     return 0;
 }
-
 
 int main()
 {
@@ -54,22 +59,22 @@ int main()
     //     std::cout << "Time taken: " << duration << " seconds" << '\n';
     // }
 
-    /*
-    Sundials test code
-    */
+    realtype reltol = 1e-4, abstol = 1e-4;  // Set tolerances
+    realtype t = 0.0;
+
+    // Create context
     SUNContext sunctx;
     void* comm = nullptr;
     int flag = SUNContext_Create(comm, &sunctx);
     if (flag != 0) {
         // handle error
+        return -1;
     }
 
-    N_Vector y = N_VNew_Serial(1, sunctx); 
-    realtype t = 0.0;
-    realtype reltol = 1e-4, abstol = 1e-4;  // Set tolerances
-
     // Initialize y
-    NV_Ith_S(y, 0) = 1.0;
+    N_Vector y = N_VNew_Serial(NEQ, sunctx);
+    NV_Ith_S(y, 0) = 0.0; // y(0) = 0
+    NV_Ith_S(y, 1) = 1.0; // y'(0) = 1
 
     /*
     For more information on the following functions, visit
@@ -78,42 +83,39 @@ int main()
     // Call CVodeCreate to create the solver memory and specify the
     // The function CVodeCreate() instantiates a CVODE solver object and specifies the solution method.
     // CV_BDF for stiff problems.
-    // CV_ADAMS for nonstiff problems 
+    // CV_ADAMS for nonstiff problems
     void *cvode_mem = CVodeCreate(CV_BDF, sunctx);
 
     // Initialize the integrator memory and specify the user's right hand
     // side function in y'=f(t,y), the inital time T0, and the initial
     // dependent variable vector y.
-    CVodeInit(cvode_mem, f, t, y);
+    CVodeInit(cvode_mem, simulate_sine, t, y);
 
     // Set the scalar relative tolerance and scalar absolute tolerance
     // cvode_mem – pointer to the CVODE memory block returned by CVodeCreate()
     CVodeSStolerances(cvode_mem, reltol, abstol);
 
-    // Call CVDense to specify the CVDENSE dense linear solver
-    SUNMatrix A = SUNDenseMatrix(1, 1, sunctx); // Create dense matrix for use in linear solves
-    SUNLinearSolver LS = SUNLinSol_Dense(y, A, sunctx); // Create dense linear solver for use in linear solves
-    CVodeSetLinearSolver(cvode_mem, LS, A); // Attach the matrix and linear solver
+    SUNMatrix A = SUNDenseMatrix(NEQ, NEQ, sunctx);  // Create dense SUNMatrix for use in linear solves
+    SUNLinearSolver LS = SUNLinSol_Dense(y, A, sunctx);  // Create dense SUNLinearSolver object
+    CVodeSetLinearSolver(cvode_mem, LS, A);  // Attach the matrix and linear solver
 
-    // Integrate over the interval, while the test function returns success
-    realtype tout = 1.0;
+    // Integrate over the interval
+    realtype tout = 0.001;
     while (t < 10.0)
     {
-        int flag = CVode(cvode_mem, tout, y, &t, CV_NORMAL);
+        flag = CVode(cvode_mem, tout, y, &t, CV_NORMAL);
         if (flag < 0)
         {
             std::cerr << "Error in integration" << std::endl;
             return -1;
         }
-        std::cout << "At t = " << t << ", y = " << NV_Ith_S(y,0) << std::endl;
-        tout += 1.0;
+        std::cout << "At t = " << t << ", y = " << NV_Ith_S(y, 0) << std::endl;
+        tout += M_PI / 2.0;
     }
 
-    // Free integrator memory
+    // Free memory
     CVodeFree(&cvode_mem);
     SUNContext_Free(&sunctx);
-
-    // Free vector
     N_VDestroy(y);
 
     return 0;
