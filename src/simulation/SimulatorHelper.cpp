61ed18d56eb9f88ba425d2c195cd86001bcf2a89
Jan-Piotraschke <piotraschke.janniklas@gmail.com>
2023-08-15 11:25:42 +0200

    finding the particle inside and outside the UV mesh

--- src/simulation/SimulatorHelper.cpp
+++ src/simulation/SimulatorHelper.cpp
@@ -8,6 +8,7 @@
 
 SimulatorHelper::SimulatorHelper(
     std::vector<VertexData>& particle_change,
+    std::vector<bool>& simulated_particles,
     int particle_count,
     Eigen::Matrix<double, Eigen::Dynamic, 2>& r_UV,
     Eigen::Matrix<double, Eigen::Dynamic, 2>& r_UV_old,
@@ -17,6 +18,7 @@ SimulatorHelper::SimulatorHelper(
     Eigen::VectorXi& n_pole_old
 )
     : particle_change(particle_change),
+    simulated_particles(simulated_particles),
     particle_count(particle_count),
     r_UV(r_UV),
     r_UV_old(r_UV_old),
@@ -40,4 +42,43 @@ void SimulatorHelper::set_new_particle_data(){
         vd.valid = false;
         vd.virtual_mesh = false;
     }
+}
+
+
+std::vector<int> SimulatorHelper::get_inside_UV_id() {
+    std::vector<int> inside_UV_id;
+    int particle_row_ID = 0;  // to keep track of the number of 'true' values we've seen so far
+    int trueCount = 0;
+
+    while (trueCount < r_UV.rows() && particle_row_ID < particle_count) {
+        if (simulated_particles[particle_row_ID] == true) {
+            Eigen::Vector2d first_two_columns = r_UV.row(particle_row_ID).head<2>();
+            if (is_inside_uv(first_two_columns)) {
+                inside_UV_id.push_back(particle_row_ID);
+            }
+            else {
+                std::cout << "particle ID " << particle_row_ID << " is outside with : " << r_UV.row(particle_row_ID) << std::endl;
+            }
+            ++trueCount;
+        }
+        ++particle_row_ID;
+    }
+
+    return inside_UV_id;
+}
+
+
+std::vector<int> SimulatorHelper::get_outside_UV_id(std::vector<int> inside_UV_id) {
+    std::vector<int> outside_UV_id;
+    int nrows = r_UV.rows();
+
+    for (int particle_row_ID = 0; particle_row_ID < nrows; ++particle_row_ID) {
+        
+        if (!(std::find(inside_UV_id.begin(), inside_UV_id.end(), particle_row_ID) != inside_UV_id.end())) {
+            std::cout << "particle ID " << particle_row_ID << " : " << r_UV.row(particle_row_ID) << std::endl;
+            outside_UV_id.push_back(particle_row_ID);
+        }
+    }
+
+    return outside_UV_id;
 }
--- src/simulation/SimulatorHelper.h
+++ src/simulation/SimulatorHelper.h
@@ -11,6 +11,7 @@ class SimulatorHelper {
 public:
     SimulatorHelper(
         std::vector<VertexData>& particle_change,
+        std::vector<bool>& simulated_particles,
         int particle_count,
         Eigen::Matrix<double, Eigen::Dynamic, 2>& r_UV,
         Eigen::Matrix<double, Eigen::Dynamic, 2>& r_UV_old,
@@ -21,9 +22,12 @@ public:
     );
 
     void set_new_particle_data();
+    std::vector<int> get_inside_UV_id();
+    std::vector<int> get_outside_UV_id(std::vector<int> inside_UV_id);
 
 private:
     std::vector<VertexData>& particle_change;
+    std::vector<bool>& simulated_particles;
     int particle_count;
     Eigen::Matrix<double, Eigen::Dynamic, 2>& r_UV;
     Eigen::Matrix<double, Eigen::Dynamic, 2>& r_UV_old;
@@ -31,4 +35,9 @@ private:
     Eigen::MatrixXd& r_3D_old;
     Eigen::VectorXi& n;
     Eigen::VectorXi& n_pole_old;
+
+    // Check if the given point r is inside the UV parametrization bounds
+    static bool is_inside_uv(const Eigen::Vector2d& r_UV) {
+        return (0 <= r_UV[0] && r_UV[0] <= 1) && (0 <= r_UV[1] && r_UV[1] <= 1);
+    };
 };
