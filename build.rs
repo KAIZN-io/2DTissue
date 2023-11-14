cac261b48c3c75a19ce6072bb808620aca953b0c
Jan-Piotraschke <piotraschke.janniklas@gmail.com>
2023-11-14 12:12:34 +0100

    added comments

--- build.rs
+++ build.rs
@@ -1,11 +1,28 @@
+//! # Build Script for Rust Project Using `autocxx`
+//!
+//! This script is responsible for setting up the build process for a Rust project
+//! which uses `autocxx` for C++ interoperability.
+//!
+//! ## Functionality
+//! - It specifies the include paths for C++ headers.
+//! - It initializes and configures the `autocxx` build process.
+//! - It sets compiler flags and compiles the generated bindings.
+//! - It includes instructions for Cargo to re-run the build script upon changes.
+
+
 fn main() -> miette::Result<()>  {
+    // Sets the include path for C++ headers
     let include_path = std::path::PathBuf::from("src");
     // let pmp_path = std::path::PathBuf::from("MeshCartographyLib/pmp-library/src");
 
+    // Initializes the `autocxx` build process
     let mut builder = autocxx_build::Builder::new("src/main.rs", &[&include_path])
         .build()?;
+
+    // Compiles the generated bindings with C++17 standards
     builder.flag_if_supported("-std=c++17").compile("autocxx-demo");
 
+    // Instructs Cargo to re-run this script if `main.rs` changes
     println!("cargo:rerun-if-changed=src/main.rs");
 
     Ok(())
